c32fbad018f43b97f91786d9ff56b04188fb4004
omar <salieomar.os@gmail.com>
2018-06-28 00:02:19 +0200

    added image slider

--- js/main.js
+++ js/main.js
@@ -8,3 +8,30 @@ function drawerToggle() {
         menu.className = "navbar";
     }
 }
+
+let slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  let i;
+  let x = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("badge");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" badgeSelect", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " badgeSelect";
+}
