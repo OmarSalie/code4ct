2c973f68e4e6b4aa9d8b487985215bba0bc0d1c1
omar <salieomar.os@gmail.com>
2018-06-28 02:32:59 +0200

    completed count up functionality on home page

--- js/main.js
+++ js/main.js
@@ -35,3 +35,39 @@ function showDivs(n) {
   x[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " badgeSelect";
 }
+
+function countUp(id, start, end, duration) {
+  // assumes integer values for start and end
+  
+  var obj = document.getElementById(id);
+  var range = end - start;
+  // no timer shorter than 50ms (not really visible any way)
+  var minTimer = 50;
+  // calc step time to show all interediate values
+  var stepTime = Math.abs(Math.floor(duration / range));
+  
+  // never go below minTimer
+  stepTime = Math.max(stepTime, minTimer);
+  
+  // get current time and calculate desired end time
+  var startTime = new Date().getTime();
+  var endTime = startTime + duration;
+  var timer;
+
+  function run() {
+      var now = new Date().getTime();
+      var remaining = Math.max((endTime - now) / duration, 0);
+      var value = Math.round(end - (remaining * range));
+      obj.innerHTML = value;
+      if (value == end) {
+          clearInterval(timer);
+      }
+  }
+  
+  timer = setInterval(run, stepTime);
+  run();
+}
+countUp("count-coffee", 0, 1050, 2000);
+countUp("count-projects", 0, 1850, 2000);
+countUp("count-girls", 0, 2000, 2000);
+countUp("count-hours", 0, 3300, 2000);
