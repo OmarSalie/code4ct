6c9736ad3d969919547369577f2312f14f520811
omar <salieomar.os@gmail.com>
2018-06-28 17:03:20 +0200

    carousel

--- js/main.js
+++ js/main.js
@@ -8,3 +8,82 @@ function drawerToggle() {
         menu.className = "navbar";
     }
 }
+
+let slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  let i;
+  let x = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("badge");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" badgeSelect", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " badgeSelect";
+}
+
+function countUp(id, start, end, duration) {
+  // assumes integer values for start and end
+  
+  var obj = document.getElementById(id);
+  var range = end - start;
+  // no timer shorter than 50ms (not really visible any way)
+  var minTimer = 50;
+  // calc step time to show all interediate values
+  var stepTime = Math.abs(Math.floor(duration / range));
+  
+  // never go below minTimer
+  stepTime = Math.max(stepTime, minTimer);
+  
+  // get current time and calculate desired end time
+  var startTime = new Date().getTime();
+  var endTime = startTime + duration;
+  var timer;
+
+  function run() {
+      var now = new Date().getTime();
+      var remaining = Math.max((endTime - now) / duration, 0);
+      var value = Math.round(end - (remaining * range));
+      obj.innerHTML = value;
+      if (value == end) {
+          clearInterval(timer);
+      }
+  }
+  
+  timer = setInterval(run, stepTime);
+  run();
+}
+
+// running the funtion for the count ups
+countUp("count-girls", 0, 780, 2000);
+countUp("count-hours", 0, 8765, 2000);
+countUp("count-coffee", 0, 3000, 2000);
+countUp("count-projects", 0, 3120, 2000);
+
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("partner-logo");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 4000); // Change image every 2 seconds
+}
